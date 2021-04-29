a9e78f96ee560f75ff85dedae841e43f343fdcf9
Tyler <tyler.j.mire@gmail.com>
2021-04-29 14:14:54 -0700

    Added cube class and drawing

--- Game.cs
+++ Game.cs
@@ -1,28 +1,27 @@
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
 
 namespace RubixSolver
 {
     public class Game1 : Game
     {
         GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+        VertexPositionNormalTexture[] cube;
 
         //Camera
-        
-        struct Camera{
-            public Vector3 Target { get; set; }
-            public Vector3 Postion { get; set; }
-            public Matrix Projection { get; set; }
-            public Matrix View { get; set; }
-            public Matrix World { get; set; }
+        //Renderer
+        public struct Renderer{
+            public BasicEffect BasicEffect; //3D equivalent to spritebatch
+            public VertexPositionColor[] TriangleVertices;
+            public VertexBuffer VertexBuffer;
         }
-
-        BasicEffect basicEffect; //3D equivalent to spritebatch
-        VertexPositionColor[] triangleVertices;
-        VertexBuffer vertexBuffer;
         
+        //Objects
+        Renderer ren;
 
-        Camera cam;
+        float angle;
         public Game1()
         {
             graphics = new GraphicsDeviceManager(this);
@@ -30,21 +29,64 @@ namespace RubixSolver
         }
         protected override void Initialize()
         {
-            cam = new Camera();
             base.Initialize();
+            //Init objects
+            ren = new Renderer();
+
+            //Setup Renderer
+            ren.BasicEffect = new BasicEffect(GraphicsDevice);
+            ren.BasicEffect.AmbientLightColor = Vector3.One;
+            ren.BasicEffect.DirectionalLight0.Enabled = true;
+            ren.BasicEffect.DirectionalLight0.DiffuseColor = Vector3.One;
+            ren.BasicEffect.DirectionalLight0.Direction = Vector3.Normalize(Vector3.One);
+            ren.BasicEffect.LightingEnabled = true;
+
+            //Setup Camera
+            Matrix projection = Matrix.CreatePerspectiveFieldOfView(MathHelper.Pi / 4.0f, (float)this.Window.ClientBounds.Width / (float)this.Window.ClientBounds.Height, 1f, 10f);
+            ren.BasicEffect.Projection = projection;
+            Matrix V = Matrix.CreateTranslation(0f,0f,-10f);
+            ren.BasicEffect.View = V;
+
+            ren.VertexBuffer = new VertexBuffer(GraphicsDevice, typeof(VertexPositionColor), 3, BufferUsage.WriteOnly);
+            ren.BasicEffect.AmbientLightColor = new Vector3(0.0f, 0.0f, 0.0f);
+
+            //Orbit
+            angle = 0;
         }
         protected override void LoadContent()
         {
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+            Cube cubeMaker = new Cube();
+            cube = cubeMaker.MakeCube();
         }
         protected override void UnloadContent()
         {
         }
         protected override void Update(GameTime gameTime)
         {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+            
+            angle = angle + 0.05f;
+            if (angle > 2 * MathHelper.Pi) angle = 0;
+            Matrix R = Matrix.CreateRotationY(angle) * Matrix.CreateRotationX(.4f);
+            Matrix T = Matrix.CreateTranslation(0.0f, 0f, 5f);
+            ren.BasicEffect.World = R * T;
             base.Update(gameTime);
         }
         protected override void Draw(GameTime gameTime)
         {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            RasterizerState rasterizerState = new RasterizerState();
+            GraphicsDevice.RasterizerState = rasterizerState;
+
+            foreach(EffectPass pass in ren.BasicEffect.CurrentTechnique.Passes)
+            {
+                pass.Apply();
+                graphics.GraphicsDevice.DrawUserPrimitives<VertexPositionNormalTexture>(PrimitiveType.TriangleList, cube, 0, 12);
+            }
+
             base.Draw(gameTime);
         }
     }
